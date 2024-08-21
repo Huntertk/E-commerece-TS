64ffbab5dad872b1938a227959c59191267b14d6
Huntertk <taufik.khan65@gmail.com>
2024-08-21 22:43:05 +0530

    home page completed

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,7 +1,32 @@
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product-card";
 
 const Home = () => {
+  const addToCartHandler = () => {
+
+  }
+
   return (
-    <div>Home</div>
+    <div className="home">
+      <section></section>
+
+
+      <h1>
+        Latest Products
+        <Link to="/search" className="findmore">More</Link>
+      </h1>
+
+      <main>
+        <ProductCard
+          productId="0198awd798a65"
+          name="Macbook"
+          price={145000}
+          stock={3}
+          photo="https://m.media-amazon.com/images/I/618d5bS2lUL._SX679_.jpg"
+          handler={addToCartHandler}
+        />
+      </main>
+    </div>
   )
 }
 
