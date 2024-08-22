b9f8697f00d474c3799373cff1871728fd8ad244
Huntertk <taufik.khan65@gmail.com>
2024-08-22 15:18:20 +0530

    shipping page ui completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import Header from './components/header';
 const Home = lazy(() => import('./pages/home'));
 const Search = lazy(() => import('./pages/search'));
 const Cart = lazy(() => import('./pages/cart'));
+const Shipping = lazy(() => import('./pages/shipping'));
 
 
 //Admin Imports
@@ -38,6 +39,11 @@ const App = () => {
           <Route path='/' element={<Home />} />
           <Route path='/search' element={<Search />} />
           <Route path='/cart' element={<Cart />} />
+
+          {/* Logged in user routes */}
+          <Route>
+            <Route path='/shipping' element={<Shipping />} />
+          </Route>
 
 
 
